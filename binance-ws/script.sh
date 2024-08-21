da178d7f675c3b2c23eeec9204672f56c1f4573f
Haiss2 <hai98th@gmail.com>
2024-08-21 19:20:26 +0700

    test_fix_api

--- binance-ws/script.sh
+++
@@ -1,5 +0,0 @@
-# For future ws testing
-go run ./ -gather-data-duration=30s
-
-# For spot ws testing
-go run ./ -gather-data-duration=30s -websocket=wss://stream.binance.com:9443/ws -file-prefix=spot
